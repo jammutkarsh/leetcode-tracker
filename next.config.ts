11bc7c71d8bf374b0bed70b12295da49e67898e4
Utkarsh Chourasia <5.utkarshchourasia@gmail.com>
2026-05-02 21:22:36 +0530

    fix: switch to wrangler.toml and fix entry-point error

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Ensure we don't use Turbopack for the production build as it's incompatible with Cloudflare
+  experimental: {
+    turbo: {
+      rules: {},
+    },
+  },
 };
 
 export default nextConfig;
